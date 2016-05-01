f77574481b82487bbb7f986c6b6d0456d2ece4ac
mtuan94 <tuan.nguyen@felizz.com>
2016-05-01 15:46:01 +0700

    first views

--- kidgames/kidgames/urls.py
+++ kidgames/kidgames/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+	url(r'^k5/', include('k5.urls')),
+	url(r'^k6/', include('k6.urls')),
+	url(r'^k7/', include('k7.urls')),
+	url(r'^k8/', include('k8.urls')),
+	url(r'^l1/', include('l1.urls')),
+	url(r'^l2/', include('l2.urls')),
     url(r'^admin/', admin.site.urls),
 ]
