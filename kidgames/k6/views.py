081724112326206c09a7ba99f1b550b3822212fb
mtuan94 <tuan.nguyen@felizz.com>
2016-05-03 00:00:03 +0700

    finish ajax load more and random question for k5

--- kidgames/k6/views.py
+++ kidgames/k6/views.py
@@ -6,22 +6,44 @@ import os
 
 from xml.dom import minidom
 import xml.etree.ElementTree as ET
+import random
+import logging
 
 dataPath = os.path.normpath(os.path.join(os.path.dirname(__file__),'..')) + '/config/data.xml'
 staticPath = os.path.normpath(os.path.join(os.path.dirname(__file__),'..')) + '/static'
-#elementtree
-#root[cat id][quess id][...]
+QuestionList = [0,1]
+
 def index(request):
     tree = ET.parse(dataPath);
     root = tree.getroot();
-    url = '/k6/q1/config.png'
+    randowmQuestion = random.choice([0,1]);
     return render(request,"k6.html",{
-    	"url": url,
-        "question_string": root[1][0][0].text,
-        "answeer": root[1][0][1].text,
-        "audio_url": root[1][0][2].text,
-        "photo1_url" : root[1][0][3][0].text,
-        "photo1_pos" : root[1][0][3][0].attrib.values,
-        "photo2_url" : root[1][0][3][1].text,
-        "photo2_pos" : root[1][0][3][1].attrib.values
+    	"url": "url",
+        "question_number": "1",
+        "question_string": root[1][randowmQuestion][0].text,
+        "answeer": root[1][randowmQuestion][1].text,
+        "audio_url": root[1][randowmQuestion][2].text,
+        "photo1_url" : root[1][randowmQuestion][3][0].text,
+        "photo1_pos" : root[1][randowmQuestion][3][0].attrib.values,
+        "photo2_url" : root[1][randowmQuestion][3][1].text,
+        "photo2_pos" : root[1][randowmQuestion][3][1].attrib.values
+        });
+
+def loadmore(request):
+    answeered = int(request.GET["answeered"]);
+    quesExist = QuestionList[:];
+    quesExist.remove(answeered);
+    randowmQuestion = random.choice(quesExist);
+    tree = ET.parse(dataPath);
+    root = tree.getroot();
+    return render(request,"k6Ajax.html",{
+        "url": "url",
+        "question_number": "2",
+        "question_string": root[1][randowmQuestion][0].text,
+        "answeer": root[1][randowmQuestion][1].text,
+        "audio_url": root[1][randowmQuestion][2].text,
+        "photo1_url" : root[1][randowmQuestion][3][0].text,
+        "photo1_pos" : root[1][randowmQuestion][3][0].attrib.values,
+        "photo2_url" : root[1][randowmQuestion][3][1].text,
+        "photo2_pos" : root[1][randowmQuestion][3][1].attrib.values
         });
