cdb85aa45183e3c298d5682a0ffbc10ba55cc916
mtuan94 <tuan.nguyen@felizz.com>
2016-05-02 15:58:44 +0700

    first done k5 one game

--- kidgames/k6/views.py
+++ kidgames/k6/views.py
@@ -9,16 +9,19 @@ import xml.etree.ElementTree as ET
 
 dataPath = os.path.normpath(os.path.join(os.path.dirname(__file__),'..')) + '/config/data.xml'
 staticPath = os.path.normpath(os.path.join(os.path.dirname(__file__),'..')) + '/static'
-
+#elementtree
+#root[cat id][quess id][...]
 def index(request):
     tree = ET.parse(dataPath);
     root = tree.getroot();
     url = '/k6/q1/config.png'
     return render(request,"k6.html",{
     	"url": url,
-        "question_string": root[0][1][0].text,
-        "photo1_url" : root[0][1][2][0].text,
-        "photo1_pos" : root[0][1][2][0].attrib.values,
-        "photo2_url" : root[0][1][2][1].text,
-        "photo2_pos" : root[0][1][2][1].attrib.values
+        "question_string": root[1][0][0].text,
+        "answeer": root[1][0][1].text,
+        "audio_url": root[1][0][2].text,
+        "photo1_url" : root[1][0][3][0].text,
+        "photo1_pos" : root[1][0][3][0].attrib.values,
+        "photo2_url" : root[1][0][3][1].text,
+        "photo2_pos" : root[1][0][3][1].attrib.values
         });
